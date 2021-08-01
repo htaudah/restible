813e56fa40907258326d0ebcede74ec287c8bd39
Hani Audah <ht.audah@gmail.com>
2021-08-02 01:43:32 +0300

    Syntax error

--- restible/urls.py
+++ restible/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import ApiEndpoint
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
