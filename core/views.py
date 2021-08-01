f2cce5763c4cfa0756207f417f127037a2a837ea
Hani Audah <ht.audah@gmail.com>
2021-08-02 01:42:45 +0300

    Added API authentication and hello

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+class ApiEndpoint(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, OAuth2!')
