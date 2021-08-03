1abb12d73bf44490af61cb7a83d16dc1614f74b5
Hani Audah <ht.audah@gmail.com>
2021-08-03 22:08:08 +0300

    Testing SSH command from django

--- core/views.py
+++ core/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 from oauth2_provider.views.generic import ProtectedResourceView
 from django.http import HttpResponse
 
+from django.conf import settings
+
+import paramiko
+
 # Create your views here.
 class ApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(settings.ANSIBLE_HOST, port=22, username=ANSIBLE_USER, password=ANSIBLE_PASSWORD)
+        client.exec_command("echo hey > /tmp/echi")
         return HttpResponse('Hello, OAuth2!')
