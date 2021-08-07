5c60d25ae1980379dd7e991e800be75eaa55a549
Hani Audah <ht.audah@gmail.com>
2021-08-07 04:07:27 +0300

    Added command to create super user

---
+++ core/commands/ensureadmin.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Ensures the admin superuser exists'
+
+    def add_arguments(self, parser):
+        parser.add_argument('password', type=str)
+        parser.add_argument('domain' type=str)
+
+    def handle(self, *args, **options):
+        try:
+            existing_admin = User.objects.get(username="admin")
+        except User.DoesNotExist:
+            User.objects.create_superuser('admin', "admin@%s" % options['domain'], options['password'])
