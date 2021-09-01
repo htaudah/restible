1af42874dc901b5ae0e0a2a6878557cb6cd97820
Hani Audah <ht.audah@gmail.com>
2021-09-02 00:09:51 +0300

    Syntax error

--- core/views.py
+++ core/views.py
@@ -22,18 +22,20 @@ class ApiEndpoint(ProtectedResourceView):
     @csrf_exempt
     def post(self, request, *args, **kwargs):
         request_data = json.loads(request.body)
-        arg_string = ""
+        #arg_string = ""
+        request_params = {}
         for key in request_data:
             if key == 'playbooks':
                 continue
-            arg_string += " %s=%s" % (key, request_data[key])
+            #arg_string += " %s=%s" % (key, request_data[key])
+            request_params[key] = request_data[key]
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(settings.ANSIBLE_HOST, port=22, username=settings.ANSIBLE_USER, password=settings.ANSIBLE_PASSWORD)
         if not ('playbooks' in request_data):
             return JsonResponse({'error': 'No playbooks were specified in request body'}, status=400)
         for playbook in request_data['playbooks']:
-            client.exec_command("%s/run_playbooks.sh %s %s" % (settings.ANSIBLE_PATH, playbook, arg_string))
+            client.exec_command("%s/run_playbooks.sh %s %s" % (settings.ANSIBLE_PATH, playbook, json.dumps(request_params)))
         return JsonResponse({'status':'running'})
 
 class HealthEndpoint(View):
